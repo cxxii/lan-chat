03142e38acfa171032e89fa14b723236062efe36
Callum Clark <44702536+cxxii@users.noreply.github.com>
2023-07-05 22:33:56 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+const serverIP = '192.168.77.194';
+
+http.listen(3000, serverIP, () => {
+  console.log('Server listening on port 3000');
+});
+
